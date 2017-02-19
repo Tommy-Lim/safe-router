6ae76910c598c4d9c9a6a6c87d61d9afad2eeb52
Tommy-Lim <tlim24@gmail.com>
2017-02-18 19:56:43 -0800

    added layers to map

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,20 @@
 window.onload = function() {
-  L.map('map', {
+
+  var mapLayer = MQ.mapLayer(), map;
+
+  // CREATE MAP
+  map = L.map('map', {
     layers: MQ.mapLayer(),
     center: [ 47.6062, -122.3321 ],
     zoom: 12
   });
+
+  // ADD MAP LAYERS
+  L.control.layers({
+    'Map': mapLayer,
+    'Hybrid': MQ.hybridLayer(),
+    'Satellite': MQ.satelliteLayer(),
+    'Dark': MQ.darkLayer(),
+    'Light': MQ.lightLayer()
+  }).addTo(map);
 };
