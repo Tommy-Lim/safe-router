e07c3d8ad39e5a2aa8f05c6fc8326daf9fdc6632
Tommy-Lim <tlim24@gmail.com>
2017-02-18 19:21:19 -0800

    added ui-router to fix component loading

---
+++ public/app/app.js
@@ -0,0 +1,20 @@
+angular.module('App', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$locationProvider',
+  function(
+    $stateProvider,
+    $urlRouterProvider,
+    $locationProvider
+  ){
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+    .state('homeState', {
+      url: '/',
+      component: 'homeComp'
+    })
+  }
+])
