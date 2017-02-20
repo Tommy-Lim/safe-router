498e7445f94a69e1b7fbe266706c299f8188c77d
Ryne Schillinger <ryneschillinger@gmail.com>
2017-02-20 12:42:52 -0800

    Returning lat&lng ranges for each leg in route

--- public/app/containers/home/home.js
+++ public/app/containers/home/home.js
@@ -7,12 +7,16 @@ angular.module('App')
 
 function HomeCompCtrl($http) {
 	var homeComp = this;
-	homeComp.start = '503 1st ave w, seattle';
+	homeComp.start = '503 1st Ave W, Seattle';
 	homeComp.destination = '1218 3rd Ave, Seattle';
+	homeComp.legRanges = [];
 
 
 	homeComp.findRoutes = function() {
+		// Reset old results
+		homeComp.legRanges = [];
 
+		// Get route result(s) from Mapquest
 		if (homeComp.start !== undefined && homeComp.start !== '' && homeComp.destination !== undefined && homeComp.destination !== '') {
 			var req = {
 				url: 'http://www.mapquestapi.com/directions/v2/route?key=qAylA8byJY9VfeyWnD4LcdGu8Z6UljKv&from=' + homeComp.start + '&to=' + homeComp.destination,
@@ -24,7 +28,37 @@ function HomeCompCtrl($http) {
 					homeComp.searchResults = null;
 				} else {
 					homeComp.searchResults = res.data.route;
-					console.log(homeComp.searchResults)
+					console.log("Search results:", homeComp.searchResults);
+
+					// Iterate through legs and find range between each one
+					var locations = homeComp.searchResults.locations;
+					var maneuvers = homeComp.searchResults.legs[0].maneuvers;
+					for (var i=1; i < maneuvers.length; i++) {
+						homeComp.legRanges.push(
+							{ latRange: {
+									begin: parseFloat(maneuvers[i-1].startPoint.lat), 
+									end: parseFloat(maneuvers[i].startPoint.lat)
+								},
+								lngRange: {
+									begin: parseFloat(maneuvers[i-1].startPoint.lng), 
+									end: parseFloat(maneuvers[i].startPoint.lng)
+								},
+							}
+						);
+					}
+					homeComp.legRanges.push(
+						{ latRange: {
+								begin: parseFloat(maneuvers[maneuvers.length-1].startPoint.lat), 
+								end: parseFloat(locations[1].latLng.lat)
+							},
+							lngRange: {
+								begin: parseFloat(maneuvers[maneuvers.length-1].startPoint.lng), 
+								end: parseFloat(locations[1].latLng.lng)
+							},
+						}
+					);
+					console.log("Leg ranges:", homeComp.legRanges);
+
 				}
 			}, function failure(res) {
 				console.log('failed');
