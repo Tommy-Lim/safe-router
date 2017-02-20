be83958973c54b323f75a6928a3826862dfa5b9e
Tommy-Lim <tlim24@gmail.com>
2017-02-20 15:57:59 -0800

    added location find and center on map

--- public/app/components/map/map.js
+++ public/app/components/map/map.js
@@ -1,37 +1,66 @@
-angular.module('App')
-.component('mapComp', {
-  templateUrl: 'app/components/map/map.html',
-  controller: MapCompCtrl,
-  controllerAs: 'mapCompCtrl'
+angular.module('App').component('mapComp', {
+    templateUrl: 'app/components/map/map.html',
+    controller: MapCompCtrl,
+    controllerAs: 'mapCompCtrl'
 });
 
-function MapCompCtrl(DirectionsServices){
-  var mapComp = this
+function MapCompCtrl(DirectionsServices) {
+    var mapComp = this
 
-  DirectionsServices.getDirections();
+    DirectionsServices.getDirections();
 
-  window.onload = function() {
-		console.log("inside comp JS")
+    window.onload = function() {
 
-	  var mapLayer = MQ.mapLayer(), map;
+        var popup = L.popup();
+        var mapLayer = MQ.mapLayer();
+        var map;
 
-	  // CREATE MAP
-	  map = L.map('map', {
-	    layers: MQ.mapLayer(),
-	    center: [ 47.6062, -122.3321 ],
-	    zoom: 12
-	  });
+        // CREATE MAP
+        map = L.map('map', {
+            layers: MQ.mapLayer(),
+            center: [
+                47.6062, -122.3321
+            ],
+            zoom: 12
+        });
 
-	  // ADD MAP LAYERS
-	  L.control.layers({
-	    'Map': mapLayer,
-	    'Hybrid': MQ.hybridLayer(),
-	    'Satellite': MQ.satelliteLayer(),
-	    'Dark': MQ.darkLayer(),
-	    'Light': MQ.lightLayer()
-	  }).addTo(map);
-	};
+        // ADD MAP LAYERS
+        L.control.layers({
+          'Map': mapLayer,
+          'Hybrid': MQ.hybridLayer(),
+          'Satellite': MQ.satelliteLayer(),
+          'Dark': MQ.darkLayer(),
+          'Light': MQ.lightLayer()
+        }).addTo(map);
 
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var latLng = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                popup.setLatLng(latLng);
+                popup.setContent('Current Location');
+                popup.openOn(map);
+
+                map.setView(latLng);
+            }, function() {
+                geolocationErrorOccurred(true, popup, map.getCenter());
+            });
+        } else {
+            //No browser support geolocation service
+            geolocationErrorOccurred(false, popup, map.getCenter());
+        }
+
+        function geolocationErrorOccurred(geolocationSupported, popup, latLng) {
+            popup.setLatLng(latLng);
+            popup.setContent(geolocationSupported
+                ? '<b>Error:</b> The Geolocation service failed.'
+                : '<b>Error:</b> This browser doesn\'t support geolocation.');
+            popup.openOn(map);
+        }
+    };
 
 }
 
