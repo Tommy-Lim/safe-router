160270200c25598c16a48f2f45c31c9c030c37aa
Tommy-Lim <tlim24@gmail.com>
2017-02-20 23:05:33 -0800

    added google maps api

--- public/app/components/map/map.js
+++ public/app/components/map/map.js
@@ -227,6 +227,19 @@ function MapCompCtrl($http, DirectionsServices) {
 
     }
 
+    mapComp.gMapsRoute = function(){
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('mapid'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
+      initMap();
+
+    }
+
     // mapComp.buildMap();
     // mapComp.centerOnCurrentLocation();
 
