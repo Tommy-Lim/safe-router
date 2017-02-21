2fd5d9f856cc98c013aa7ec00030e975f9f5554f
Tommy-Lim <tlim24@gmail.com>
2017-02-20 16:36:24 -0800

    removed additional map layers and added local scripts

--- public/app/components/map/map.js
+++ public/app/components/map/map.js
@@ -4,64 +4,11 @@ angular.module('App').component('mapComp', {
     controllerAs: 'mapCompCtrl'
 });
 
-function MapCompCtrl(DirectionsServices) {
+function MapCompCtrl($http, DirectionsServices) {
     var mapComp = this
 
     DirectionsServices.getDirections();
 
-    window.onload = function() {
-
-        var popup = L.popup();
-        var mapLayer = MQ.mapLayer();
-        var map;
-
-        // CREATE MAP
-        map = L.map('map', {
-            layers: MQ.mapLayer(),
-            center: [
-                47.6062, -122.3321
-            ],
-            zoom: 12
-        });
-
-        // ADD MAP LAYERS
-        L.control.layers({
-          'Map': mapLayer,
-          'Hybrid': MQ.hybridLayer(),
-          'Satellite': MQ.satelliteLayer(),
-          'Dark': MQ.darkLayer(),
-          'Light': MQ.lightLayer()
-        }).addTo(map);
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var latLng = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                popup.setLatLng(latLng);
-                popup.setContent('Current Location');
-                popup.openOn(map);
-
-                map.setView(latLng);
-            }, function() {
-                geolocationErrorOccurred(true, popup, map.getCenter());
-            });
-        } else {
-            //No browser support geolocation service
-            geolocationErrorOccurred(false, popup, map.getCenter());
-        }
-
-        function geolocationErrorOccurred(geolocationSupported, popup, latLng) {
-            popup.setLatLng(latLng);
-            popup.setContent(geolocationSupported
-                ? '<b>Error:</b> The Geolocation service failed.'
-                : '<b>Error:</b> This browser doesn\'t support geolocation.');
-            popup.openOn(map);
-        }
-    };
-
 }
 
-MapCompCtrl.$inject = ['DirectionsServices']
+MapCompCtrl.$inject = ['$http', 'DirectionsServices']
--- public/js/map.js
+++ public/js/map.js
@@ -1,1 +1,58 @@
+// CREATE MAP ON LOAD
+window.onload = function() {
+  console.log("building map...")
 
+    var popup = L.popup();
+    var map;
+    console.log("DOM loaded and building map")
+
+    // CREATE MAP
+    map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [
+            47.6062, -122.3321
+        ],
+        zoom: 13
+    });
+
+    // ADD MAP LAYERS
+    // L.control.layers({
+    //   'Map': MQ.mapLayer(),
+    //   'Hybrid': MQ.hybridLayer(),
+    //   'Satellite': MQ.satelliteLayer(),
+    //   'Dark': MQ.darkLayer(),
+    //   'Light': MQ.lightLayer()
+    // }).addTo(map);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log(position.coords)
+          console.log(position.coords.latitude, position.coords.longitude)
+            var latLng = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            console.log(latLng)
+
+            popup.setLatLng(latLng);
+            popup.setContent('Current Location');
+            popup.openOn(map);
+
+            map.setView(latLng, 13);
+        }, function() {
+            geolocationErrorOccurred(true, popup, map.getCenter());
+        });
+    } else {
+        //No browser support geolocation service
+        geolocationErrorOccurred(false, popup, map.getCenter());
+    }
+
+    function geolocationErrorOccurred(geolocationSupported, popup, latLng) {
+        popup.setLatLng(latLng);
+        popup.setContent(geolocationSupported
+            ? '<b>Error:</b> The Geolocation service failed.'
+            : '<b>Error:</b> This browser doesn\'t support geolocation.');
+        popup.openOn(map);
+    }
+};
