b675056e7a5fdf2ce364d5c942285eaec5ac7bc1
Tommy-Lim <tlim24@gmail.com>
2017-02-21 20:26:09 -0800

    added traffic layer

--- public/app/components/map/map.js
+++ public/app/components/map/map.js
@@ -54,8 +54,11 @@ function MapCompCtrl($http, DirectionsServices, CrimeService) {
             // CREATE GOOGLE MAP WITH LAT/LNG
             mapComp.mapid = new google.maps.Map(document.getElementById('mapid'), {
                 center: latLng,
-                zoom: 12
+                zoom: 14
             });
+
+            var trafficLayer = new google.maps.TrafficLayer();
+            trafficLayer.setMap(mapComp.mapid);
 
             // SET THE DIRECTIONS DISPLAY TO BE ON THE MAP AND ASSIGN THE DIRECTIONS TEXT TO DIRECTIONS PANEL
             mapComp.directionsDisplay.setMap(mapComp.mapid);
@@ -72,7 +75,7 @@ function MapCompCtrl($http, DirectionsServices, CrimeService) {
 
             // WHEN ROUTE OPTION IS CHANGED, RETURN DIRECTIONS
             google.maps.event.addListener(mapComp.directionsDisplay, 'routeindex_changed', function() {
-                mapScope.removeMarkers();
+                // mapScope.removeMarkers();
                 mapScope.removeBox();
                 // CURRENT ROUTE INDEX
                 console.log("SELECTED ROUTE INDEX: ", this.getRouteIndex());
@@ -80,7 +83,7 @@ function MapCompCtrl($http, DirectionsServices, CrimeService) {
                 newRoutes = this.getDirections().routes
                 console.log("SELECTED ROUTE: ", newRoutes);
                 mapComp.latLngArray = mapScope.polylinesToLatLngArr(newRoutes)
-                mapScope.addMarkers();
+                // mapScope.addMarkers();
                 mapScope.getRouteBox();
                 mapScope.getCrimes();
             });
@@ -242,6 +245,7 @@ function MapCompCtrl($http, DirectionsServices, CrimeService) {
         console.log("CRIMES: ", data.result);
         mapComp.crimes = data.result;
         mapScope.plotCrimes();
+        mapScope.findMatches();
       });
     }
 
@@ -253,10 +257,46 @@ function MapCompCtrl($http, DirectionsServices, CrimeService) {
       console.log("DATA for heatmap:", mapComp.heatMapData)
       console.log("DATA for heatmap:", mapComp.heatMapData[0].lat(),  mapComp.heatMapData[0].lng())
       var heatMap = new google.maps.visualization.HeatmapLayer({
-        data: mapComp.heatMapData
+        data: mapComp.heatMapData,
+        radius: 15,
+        // gradient: [
+        //   'rgba(0, 255, 255, 0)',
+        //   'rgba(0, 255, 255, 1)',
+        //   'rgba(0, 191, 255, 1)',
+        //   'rgba(0, 127, 255, 1)',
+        //   'rgba(0, 63, 255, 1)',
+        //   'rgba(0, 0, 255, 1)',
+        //   'rgba(0, 0, 223, 1)',
+        //   'rgba(0, 0, 191, 1)',
+        //   'rgba(0, 0, 159, 1)',
+        //   'rgba(0, 0, 127, 1)',
+        //   'rgba(63, 0, 91, 1)',
+        //   'rgba(127, 0, 63, 1)',
+        //   'rgba(191, 0, 31, 1)',
+        //   'rgba(255, 0, 0, 1)'
+        // ]
       })
 
       heatMap.setMap(mapComp.mapid);
+    }
+
+    mapComp.findMatches = function(){
+      routes = mapComp.latLngArray;
+      crimes = mapComp.crimes;
+
+      crimes = crimes.map(function(crime){
+        return {lat: crime.latitude.toFixed(3), lng: crime.longitude.toFixed(3)}
+      })
+
+      routes = routes.map(function(route){
+        return route.map(function(coordinate){
+          return {lat: coordinate.lat.toFixed(3), lng: coordinate.lng.toFixed(3)}
+        })
+      })
+
+      console.log("EDITED CRIMES: ", crimes)
+      console.log("EDITED ROUTES: ", routes)
+
     }
 
 
