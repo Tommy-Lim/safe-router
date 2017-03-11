80c68beff1a187302683791d070a47f806509710
Tommy-Lim <tlim24@gmail.com>
2017-03-10 16:37:02 -0800

    put all scripts in async defer and removed callback init

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+function gm_authFailure() { /* Code */ };
+
 $(document).ready(function(){
   console.log("Welcome to Beacon!");
-  
+
   $("input[type='checkbox']").on("click", function() {
   	if ($(this).checked === false) {
   		$(this).attr("checked", "checked");
